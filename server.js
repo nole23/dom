b52f8f417e0a75f84658c354884dd51db77a3a3e
nole23 <nole0223@gmail.com>
2019-11-16 16:50:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+var express = require('express');
+const path = require('path');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
 
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+var port = process.env.PORT || 8080;
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
+});
+
+app.use('/static', express.static('public'));
+app.use(express.static(__dirname + '/dist/twoWay'));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/twoWay/index.html'));
+})
+
+var http = require('http').Server(app);
+http.listen(port, () => console.log(`Listening on ${ port }`));
